babc19e87eab84b816d72c5a4e3e8a6bd8e6a89a
Waley <44916399+DarkRainX3@users.noreply.github.com>
2018-11-11 15:12:56 -0700

    multiply func

---
+++ More.js
@@ -0,0 +1,9 @@
+var thisfunc = function() {
+  var result = 6*7;
+  console.log("6 times 7 is ", result);
+}
+function multiply (a, b) {
+  console.log("a * b is: "+ a*b);
+  return a*b;
+}
+multiply(4,5);
