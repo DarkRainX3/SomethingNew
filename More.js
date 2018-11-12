470c6c87a309762081876b53a27daf0a5fb902b4
Waley <44916399+DarkRainX3@users.noreply.github.com>
2018-11-11 19:18:45 -0700

    Added Snakes

--- More.js
+++ More.js
@@ -1,15 +1,17 @@
-var thisfunc = function() {
-  var result = 6*7;
+/* eslint-disable quotes */
+/* eslint semi: ["error", "always"] */
+var thisfunc = function () {
+  var result = 6 * 7;
   console.log("6 times 7 is ", result);
 }
 function multiply (a, b) {
-  var result = a*b;
+  var result = a * b;
   console.log("a * b is: ", result);
-  return a*b;
+  return result;
 }
-multiply(4,5);
+multiply(4, 5);
 
-function subtract() {
-  var result = 5-4;
+function subtract () {
+  var result = 5 - 4;
   console.log("5-4 is: ", result);
 }
